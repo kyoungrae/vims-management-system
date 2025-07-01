bed54216feb7ce70e38531d839bf94b622a15b39
ikyoungtae <go66go66@naver.com>
2025-07-01 11:39:38 +0900

    init

--- src/main/resources/static/common/js/message/CommonGroupMessage.js
+++ src/main/resources/static/common/js/message/CommonGroupMessage.js
@@ -1,8 +1,12 @@
 <!--const Message = {};-->
 <!--Message.Label = {};-->
 <!--Message.Label.Array = {};-->
+Message.Label.Array["COMMON_GROUP.TITLE_DETAIL"] = "그룹상세";
+Message.Label.Array["COMMON_GROUP.TITLE_LIST"] = "그룹설정";
+Message.Label.Array["COMMON_GROUP.TITLE_MODIFY"] = "그룹수정";
+Message.Label.Array["COMMON_GROUP.TITLE_REGISTER"] = "그룹등록";
 
-Message.Label.Array["COMMON_GROUP.CHECK.GROUP_ID"] = "(을)를 입력해 주세요";
+Message.Label.Array["COMMON_GROUP.CHECK.GROUP_ID"] = "그룹아이디(을)를 입력해 주세요";
 Message.Label.Array["COMMON_GROUP.CHECK.GROUP_NAME"] = "그룹이름(을)를 입력해 주세요";
 Message.Label.Array["COMMON_GROUP.CHECK.GROUP_LEVEL"] = "그룹레벨(을)를 입력해 주세요";
 Message.Label.Array["COMMON_GROUP.CHECK.TOP_GROUP_ID"] = "상위 그룹아이디(을)를 입력해 주세요";
@@ -13,7 +17,7 @@ Message.Label.Array["COMMON_GROUP.CHECK.SYSTEM_UPDATE_DATE"] = "수정일자(을
 Message.Label.Array["COMMON_GROUP.CHECK.SYSTEM_UPDATE_USERID"] = "수정자ID(을)를 입력해 주세요";
 
 
-Message.Label.Array["COMMON_GROUP.GROUP_ID"] = "";
+Message.Label.Array["COMMON_GROUP.GROUP_ID"] = "그룹아이디";
 Message.Label.Array["COMMON_GROUP.GROUP_NAME"] = "그룹이름";
 Message.Label.Array["COMMON_GROUP.GROUP_LEVEL"] = "그룹레벨";
 Message.Label.Array["COMMON_GROUP.TOP_GROUP_ID"] = "상위 그룹아이디";
